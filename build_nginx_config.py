ce674f1632645afe95dabc047cb8a80716a9c6b1
Victor Delgado <vdel26@gmail.com>
2014-08-19 17:30:22 +0200

    run replacements in downloaded nginx files

--- build_nginx_config.py
+++ build_nginx_config.py
@@ -1,9 +1,19 @@
+from fnmatch import fnmatch
 import urllib2
 import zipfile
 import os
+import sys
+import fileinput
 
 CONFIGBUNDLE = 'nginx-config.zip'
 ENDPOINT = '/admin/api/nginx.zip?provider_key='
+REPLACEMENTS = {
+    '# daemon off;' : 'daemon off;',
+    '# error_log stderr notice;' : 'error_log stderr;',
+    'listen 80;' : 'listen ${{PORT}} ;',
+    'access_by_lua_file lua_tmp.lua;' : 'access_by_lua_file nginx_3scale_access.lua;',
+    'server_name' : '# server_name'
+}
 
 def build_provider_url():
     provider_key = os.environ['THREESCALE_PROVIDER_KEY']
@@ -18,10 +28,28 @@ def download_config_bundle(url):
     with zipfile.ZipFile(CONFIGBUNDLE, 'r') as z:
         z.extractall()
 
-def modify_config_for_heroku():
-    pass
+def find_file(namepattern):
+    for file in os.listdir('.'):
+        if fnmatch(file, namepattern):
+            return file
+
+def modify_config_for_heroku(nginx_conf_file):
+    for line in fileinput.input(nginx_conf_file, inplace=True):
+        for original, replacement in REPLACEMENTS.iteritems():
+            if original in line:
+                line = line.replace(original, replacement)
+        sys.stdout.write(line)
+
 
 url = build_provider_url()
 print 'DEBUG: url %s' % (url)
+
 download_config_bundle(url)
-print 'DEBUG: done'
+print 'DEBUG: downloaded'
+
+nginxconf = find_file('nginx_*.conf')
+nginxlua = find_file('nginx_*.lua')
+modify_config_for_heroku(nginxconf)
+os.rename(nginxconf, 'nginx.conf')
+os.rename(nginxlua, 'nginx_3scale_access.lua')
+print 'DEBUG: done!'
